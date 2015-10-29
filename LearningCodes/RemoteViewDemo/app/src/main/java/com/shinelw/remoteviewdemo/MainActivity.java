edde584104eb97b5a72672f58d2098114fc27ebc
shenlw <shen_liangwei@163.com>
2015-10-29 21:32:15 +0800

    terrible

--- LearningCodes/RemoteViewDemo/app/src/main/java/com/shinelw/remoteviewdemo/MainActivity.java
+++ LearningCodes/RemoteViewDemo/app/src/main/java/com/shinelw/remoteviewdemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.shinelw.remoteviewdemo;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Notification notification = new Notification();
+        notification.icon = R.mipmap.ic_launcher;
+        notification.tickerText = "hello world";
+        notification.when = System.currentTimeMillis();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notification.contentView = new RemoteViews(getPackageName(), R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, notification);
     }
 
 }
