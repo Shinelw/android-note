758c60b25769e1abae4ce2340bbc775394f358c6
shenlw <shen_liangwei@163.com>
2015-10-29 21:51:11 +0800

    remoteview

--- LearningCodes/RemoteViewDemo/app/src/main/java/com/shinelw/remoteviewdemo/MainActivity.java
+++ LearningCodes/RemoteViewDemo/app/src/main/java/com/shinelw/remoteviewdemo/MainActivity.java
@@ -20,12 +20,14 @@ public class MainActivity extends Activity {
         notification.tickerText = "hello world";
         notification.when = System.currentTimeMillis();
         notification.flags = Notification.FLAG_AUTO_CANCEL;
-        notification.contentView = new RemoteViews(getPackageName(), R.layout.activity_main);
-
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.activity_main);
+        notification.contentView = remoteViews;
+        notification.contentIntent = pendingIntent;
         manager.notify(1, notification);
+
     }
 
 }
