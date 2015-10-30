cb3a09e9e8999006083ac56af50ecf77ec6ad68d
shenlw <shen_liangwei@163.com>
2015-10-30 20:24:57 +0800

    add nineoldandroids

--- LearningCodes/AnimationDemo/app/src/main/java/com/shinelw/animationdemo/MainActivity.java
+++ LearningCodes/AnimationDemo/app/src/main/java/com/shinelw/animationdemo/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
             }
         });
         listview = (ListView) findViewById(R.id.list);
-       listview.setAdapter(new BaseAdapter() {
+        listview.setAdapter(new BaseAdapter() {
            @Override
            public int getCount() {
                return 10;
@@ -54,7 +54,7 @@ public class MainActivity extends Activity {
                textview.setText("item" + position);
                return view;
            }
-       });
+        });
     }
 
 }
