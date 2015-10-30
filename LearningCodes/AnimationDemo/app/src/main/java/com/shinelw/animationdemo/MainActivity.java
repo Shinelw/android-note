5f55b2b04bfca91f8dd6a56e8b50c48db426da63
shenlw <shen_liangwei@163.com>
2015-10-30 21:03:18 +0800

    animation 初窥

--- LearningCodes/AnimationDemo/app/src/main/java/com/shinelw/animationdemo/MainActivity.java
+++ LearningCodes/AnimationDemo/app/src/main/java/com/shinelw/animationdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shinelw.animationdemo;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,11 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ArgbEvaluator;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimator;
 
 public class MainActivity extends Activity {
     private Button button;
@@ -21,15 +27,35 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
         final Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation_test);
-        animation.setFillAfter(false);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Rotate3dAnimation rotate3dAnimation = new Rotate3dAnimation(0, 720, button.getX() / 2, button.getY() / 2, 0, false);
                 rotate3dAnimation.setDuration(2000);
-                button.startAnimation(rotate3dAnimation);
+                ObjectAnimator.ofFloat(button, "translationY", -button.getHeight()).start();
+                ValueAnimator colorAnim = ObjectAnimator.ofInt(button, "backgroundColor", Color.RED, Color.BLUE);
+                colorAnim.setDuration(3000);
+                colorAnim.setEvaluator(new ArgbEvaluator());
+                colorAnim.setRepeatCount(ValueAnimator.INFINITE);
+                colorAnim.setRepeatMode(ValueAnimator.REVERSE);
+                colorAnim.start();
+
+                //旋转、平移、缩放、透明度改变
+                AnimatorSet set = new AnimatorSet();
+                set.playTogether(
+                        ObjectAnimator.ofFloat(button, "rotationX", 0, 360),
+                        ObjectAnimator.ofFloat(button, "rotationY", 0, 180),
+                        ObjectAnimator.ofFloat(button, "translationX", 0, 90),
+                        ObjectAnimator.ofFloat(button, "translationY", 0, 90),
+                        ObjectAnimator.ofFloat(button, "scaleX", 1, 1.5f),
+                        ObjectAnimator.ofFloat(button, "scaleY", 1, 1.5f),
+                        ObjectAnimator.ofFloat(button, "alpha", 1, 0.25f, 1)
+
+                );
+                set.setDuration(5* 1000).start();
             }
         });
+
         listview = (ListView) findViewById(R.id.list);
         listview.setAdapter(new BaseAdapter() {
            @Override
