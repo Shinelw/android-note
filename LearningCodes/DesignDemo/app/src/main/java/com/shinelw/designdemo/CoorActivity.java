a6d3c202e0ed3419549c1441a62dd389afb1389d
shenlw <shen_liangwei@163.com>
2015-11-03 01:11:14 +0800

    beautiful demo

--- LearningCodes/DesignDemo/app/src/main/java/com/shinelw/designdemo/CoorActivity.java
+++ LearningCodes/DesignDemo/app/src/main/java/com/shinelw/designdemo/CoorActivity.java
@@ -1,10 +1,12 @@
 package com.shinelw.designdemo;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 /**
@@ -13,19 +15,25 @@ import android.view.View;
 public class CoorActivity extends AppCompatActivity {
     private FloatingActionButton button;
     private CoordinatorLayout rootLayout;
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coor);
         button = (FloatingActionButton) findViewById(R.id.button);
         rootLayout = (CoordinatorLayout) findViewById(R.id.layout_root);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.colllayout);
+        collapsingToolbarLayout.setTitle("Shinelw");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar.make(rootLayout, "hi,everyone", Snackbar.LENGTH_LONG).setAction("好的", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        
+
                     }
                 }).show();
             }
