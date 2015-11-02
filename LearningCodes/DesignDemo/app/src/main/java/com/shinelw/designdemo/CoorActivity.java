307dd0351f12f9c87f9b0351ddd04345d5019d28
shenlw <shen_liangwei@163.com>
2015-11-03 01:29:57 +0800

    add 沉浸式

--- LearningCodes/DesignDemo/app/src/main/java/com/shinelw/designdemo/CoorActivity.java
+++ LearningCodes/DesignDemo/app/src/main/java/com/shinelw/designdemo/CoorActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by shinelw on 11/3/15.
@@ -21,6 +23,11 @@ public class CoorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coor);
+        if (android.os.Build.VERSION.SDK_INT > 18) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         button = (FloatingActionButton) findViewById(R.id.button);
         rootLayout = (CoordinatorLayout) findViewById(R.id.layout_root);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
