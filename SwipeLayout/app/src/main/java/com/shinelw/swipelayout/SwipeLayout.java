a8e43255c7573665e68e7c771608e5f02b92e824
shenlw <shen_liangwei@163.com>
2016-02-05 22:15:24 +0800

    swipelayout

---
+++ SwipeLayout/app/src/main/java/com/shinelw/swipelayout/SwipeLayout.java
@@ -0,0 +1,137 @@
+package com.shinelw.swipelayout;
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by shinelw on 2/5/16.
+ */
+public class SwipeLayout extends LinearLayout {
+
+    private View contentView;
+    private View actionView;
+    private int dragDistance;
+    private ViewDragHelper viewDragHelper;
+    private int draggedX;
+    private final double AUTO_OPEN_SPEED_LIMIT = 800.0;
+
+
+    public SwipeLayout(Context context) {
+        super(context, null);
+        viewDragHelper = ViewDragHelper.create(this, new DragHelperCallBack());
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs, -1);
+        viewDragHelper = ViewDragHelper.create(this, new DragHelperCallBack());
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        viewDragHelper = ViewDragHelper.create(this, new DragHelperCallBack());
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        contentView = getChildAt(0);
+        actionView = getChildAt(1);
+        actionView.setVisibility(GONE);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(viewDragHelper.shouldInterceptTouchEvent(ev)) {
+            return true;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        viewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if(viewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        dragDistance = actionView.getMeasuredHeight();
+
+    }
+
+    private class DragHelperCallBack extends ViewDragHelper.Callback {
+
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return contentView == child || actionView == child;
+        }
+
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            draggedX = left;
+            if (changedView == contentView) {
+                actionView.offsetLeftAndRight(dx);
+            } else {
+                contentView.offsetLeftAndRight(dx);
+            }
+            if (actionView.getVisibility() == View.GONE) {
+                actionView.setVisibility(View.VISIBLE);
+            }
+            invalidate();
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            if (child == contentView) {
+                final int leftBound = getPaddingLeft();
+                final int minLeftBound = -leftBound - dragDistance;
+                final int newLeft = Math.min(Math.max(minLeftBound, left), 0);
+                return newLeft;
+            } else {
+                final int minLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() - dragDistance;
+                final int maxLeftBound = getPaddingLeft() + contentView.getMeasuredWidth() + getPaddingRight();
+                final int newLeft = Math.min(Math.max(left, minLeftBound), maxLeftBound);
+                return newLeft;
+            }
+        }
+
+        @Override
+        public int getViewHorizontalDragRange(View child) {
+            return dragDistance;
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            boolean settleToOpen = false;
+            if (xvel > AUTO_OPEN_SPEED_LIMIT) {
+                settleToOpen = false;
+            } else if (xvel < -AUTO_OPEN_SPEED_LIMIT) {
+                settleToOpen = true;
+            } else if (draggedX <= -dragDistance / 2) {
+                settleToOpen = true;
+            } else if (draggedX > -dragDistance / 2) {
+                settleToOpen = false;
+            }
+
+            final int settleDestX = settleToOpen ? -dragDistance : 0;
+            viewDragHelper.smoothSlideViewTo(contentView, settleDestX, 0);
+            ViewCompat.postInvalidateOnAnimation(SwipeLayout.this);
+        }
+    }
+}
